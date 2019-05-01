facbc6c5ba7864b5c239caedabf5a763fa32afa2
Roman Krivtsov <rkrivtsov@thinkport.digital>
2019-05-01 14:55:18 +0200

    first

---
+++ src/main/java/App.java
@@ -0,0 +1,8 @@
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+public class App {
+
+    public static void main(String[] args) {
+        YAMLFactory yf =  new YAMLFactory();
+    }
+}
